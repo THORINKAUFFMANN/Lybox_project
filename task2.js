15936752563e70b21fe7d4efad92ecb99c061354
THORINKAUFFMANN <marion.kauffmann@epitech.eu>
2022-05-25 06:09:48 +0200

    [Fix] task3.py

--- task2.js
+++ task2.js
@@ -1,9 +1,10 @@
 const paragraph = 'Investissement locatif T2 loué 496€/mois dont 26€ de charges.';
 
 const searchTerm = 'loué';
+const searchDate = '€/mois';
+
 const indexOf = paragraph.indexOf(searchTerm);
 
-const index = paragraph.indexOf(searchTerm, (indexOf + 1))
+const index = paragraph.indexOf(searchDate, (indexOf + 1))
 
-console.log(`The index of the 2nd "${searchTerm}" is ${index}`);
-// expected output: "The index of the 2nd "dog" is 52"
+console.log(index);
