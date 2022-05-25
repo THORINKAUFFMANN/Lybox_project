17682f49943baf4edab0f7c6bf7873a79b44e787
THORINKAUFFMANN <marion.kauffmann@epitech.eu>
2022-05-25 05:40:13 +0200

    [ADD] task3.py

---
+++ task3.py
@@ -0,0 +1,31 @@
+number1 = 471
+number2 = 480
+
+def calculate(nb):
+    res = nb
+    i = 10
+
+    while((nb / i) >= 10):
+        i = i * 10
+    while (i != 1):
+        res = res + ((nb / i) % 10)
+        i = i / 10
+    return res
+
+def manage(nb1, nb2):
+    joinpt = 0
+
+    while(nb1 != nb2):
+        nb1 = calculate(nb1)
+        nb2 = calculate(nb2)
+    joinpt = calculate(nb1)
+    return joinpt
+
+if(0 < number1 & number1< 20000000 & 0 < number2 & number2 < 20000000) :
+    if(manage(number2, number1) > 0 & manage(number1, number2) < 20000000):
+        joinpt = manage(number1, number2)
+        print(joinpt)
+    else :
+        print("the rule was not respected2")
+else :
+    print("the rule was not respected")
